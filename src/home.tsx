b8048f63fc87449544994ab8c45a6e4cbea3b511
Liam <195730031+liam-621@users.noreply.github.com>
2025-11-29 02:17:12 +1100

    start work on home screen

---
+++ src/home.tsx
@@ -0,0 +1,7 @@
+type Feature = { title: string; description: string }
+
+const features: Feature[] = [
+    { title: 'Finish the lyric', description: 'Test your knowledge of song lyrics by completing the missing words.' },
+    { title: 'Guess the song', description: 'Identify songs based on short audio clips' },
+]
+
